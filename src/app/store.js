1fd5c790376c6474c1b11539ae4b08090edeeee9
coder7475 <robiulhossain7475@gmail.com>
2024-01-19 20:28:11 +0600

    feat: add todos slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './../features/counterSlice/counterSlice';
+import todosReducer from '../features/todosSlice/todosSlice';
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    todos: todosReducer,
   },
 })
 
